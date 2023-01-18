96ca15ee64e4740be56ff96807b15f1ac4033914
Slamet Abtohi <slametabtohi18@gmail.com>
2023-01-18 09:27:39 +0700

    Update and rename grouping array 2D and sum based on 1 criteria.osts to grouping array 2D and sum based on 1 criteria.ts

--- Grouping and Sum/grouping array 2D and sum based on 1 criteria.ts
+++ Grouping and Sum/grouping array 2D and sum based on 1 criteria.ts
@@ -1,3 +1,13 @@
+/*
+Melakukan grouping data array 2D\
+Contoh :
+data = [["proj1",10],["proj2",15],["proj1",3]]
+out = groupAndSum(data)
+
+out:
+[["proj1",13],["proj2",15]]
+
+*/
 function groupAndSum(data: string[][]) {
   const groupedAndSummed: { [key: string]: number } = {};
   const result: string[][] = [];
